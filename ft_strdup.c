a6f12a6af44cc93f306af321704cd854d3d2e92f
Laurie Aubry <laaubry@f6r9s11.clusters.42paris.fr>
2025-11-16 22:52:33 +0100

    norminette good

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 18:48:29 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/16 22:47:36 by laaubry          ###   ########.fr       */
+/*   Updated: 2025/11/16 22:52:15 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strdup(const char *s1)
 	size_t	i;
 	size_t	len;
 	char	*s2;
+
 	len = ft_strlen(s1);
 	i = 0;
-
 	s2 = malloc(sizeof(len));
 	if (!s2 || !len)
 		return (NULL);
-	while(s1[i])
+	while (s1[i])
 	{
 		s2[i] = s1[i];
 		i++;
@@ -32,6 +32,7 @@ char	*ft_strdup(const char *s1)
 	return (s2);
 }
 
+/*
 #include <stdio.h>
 int	main()
 {
@@ -47,3 +48,4 @@ int	main()
 		printf("Error\n");
 	return 0;
 }
+*/
