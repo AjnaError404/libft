0703fec1eccfa772a46c065bec0a780f7c13b898
Laurie Aubry <laaubry@f6r9s11.clusters.42paris.fr>
2025-11-16 18:47:43 +0100

    atoi added

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,39 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/11 14:14:43 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/16 18:23:33 by laaubry          ###   ########.fr       */
+/*   Created: 2025/11/16 18:23:50 by laaubry           #+#    #+#             */
+/*   Updated: 2025/11/16 18:46:59 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-// not done yet
-
-size_t	ft_strlcat(char *dest, const char *src, size_t siz)
+int	ft_atoi(const char *str)
 {
-	size_t	i;
-	size_t src_len;
-	size_t dest_len;
+	int	nbr;
+	int	i;
+	int sign;
 
+	nbr = 0;
 	i = 0;
-	src_len = ft_strlen(src);
-	dest_len = ft_strlen(dest);
-	if (!dest)
-		return (0);
-	if (!src)
-		return (0);
-	if (dest_len == siz)
-		return (dest_len + src_len);
+	sign = 1;
 
-	while (i < siz)
+	while ((str[i] >= 9 && str[i] <= 11) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
 	{
-		dest[dest_len + i] = src[i];
+		sign = -1;
 		i++;
 	}
-	return (src_len + dest_len);
+	if (str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = str[i] * 10 - '0';
+		i++;
+	}
+	return (nbr * sign)
 }
-
