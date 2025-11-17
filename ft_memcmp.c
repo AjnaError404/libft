f1016ac94a2df5bd8f8f5d3d181b539eb3ad8e5c
Laurie Aubry <laaubry@f6r6s12.clusters.42paris.fr>
2025-11-17 20:21:54 +0100

    memcmp perfect

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 17:11:04 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/17 16:02:02 by laaubry          ###   ########.fr       */
+/*   Updated: 2025/11/17 20:14:30 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,32 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
+	str1 = (unsigned char *)(s1);
+	str2 = (unsigned char *)(s2);
 	i = 0;
-	while (*(unsigned char *)(s1 + i) && *(unsigned char *)(s2 + i)
-			&& *(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i)
-			&& i < n)
+	if (!n)
+		return (0);
+	while (str1[i] == str2[i] && i < n - 1)
 		i++;
-	return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+	return (str1[i] - str2[i]);
 }
+
+/*
+#include <string.h>
+#include <stdio.h>
+
+int	main()
+{
+	char	*str = "zyxbcdefgh";
+	char	*str2 = "abcdefgxyz";
+	size_t	n = 0;
+
+	printf("my ft = %d\n", ft_memcmp(str, str2, n));
+	printf("real = %d\n", memcmp(str, str2, n));
+	return (0);
+}
+*/
