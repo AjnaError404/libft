b0370638e7267538fc1e5953a887492197704cb0
Laurie Aubry <laaubry@f6r6s12.clusters.42paris.fr>
2025-11-17 19:34:24 +0100

    memchr clean

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:36:07 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/16 16:59:22 by laaubry          ###   ########.fr       */
+/*   Updated: 2025/11/17 19:26:29 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	cc;
 
+	str = (unsigned char *)s;
+	cc = (unsigned char)c;
 	i = 0;
-	if (!s || !c)
-		return (NULL);
-	while (i < n && *(unsigned char *)(s + i) != c)
+	while (i < n)
+	{
+		if (str[i] == cc)
+			return ((void *)(str + i));
 		i++;
-	return ((void *)(s + i));
+	}
+	return (NULL);
 }
