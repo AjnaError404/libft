bf2c83b8327d4fd6e1c92111b1a5213758a4d865
Laurie Aubry <laaubry@f6r6s12.clusters.42paris.fr>
2025-11-17 15:37:17 +0100

    memmove and strlcat perfect

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 12:42:21 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/16 22:27:48 by laaubry          ###   ########.fr       */
+/*   Updated: 2025/11/17 15:36:47 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,41 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	d = (unsigned char *)dst;
 	s = (const unsigned char *)src;
 	i = 0;
+	if (dst == src || len == 0)
+		return (dst);
 	if (d > s && d < s + len)
 	{
-		while (i < len - 1)
-		{
-			d[i] = s[i];
-			i--;
-		}
+		while (len-- > 0)
+			d[len] = s[len];
 	}
 	else
 	{
 		while (i < len)
-		d[i] = s[i];
-		i++;
+		{
+			d[i] = s[i];
+			i++;
+		}
 	}
 	return (d);
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int	main()
+{
+	char *src = "this is a good nyancat !\r\n";
+ 	char dst1[0xF0];
+ 	char dst2[0xF0];
+ 	int size = strlen(src);
+ 
+ 	memmove(dst1, src, size);
+ 	ft_memmove(dst2, src, size);
+ 	if (!memcmp(dst1, dst2, size))
+ 		printf("success\n");
+ 	else
+		printf("failed\n");
+	return 0;
+}
+*/
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,34 +6,36 @@
 /*   By: laaubry <laaubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 14:14:43 by laaubry           #+#    #+#             */
-/*   Updated: 2025/11/16 22:30:00 by laaubry          ###   ########.fr       */
+/*   Updated: 2025/11/17 12:11:41 by laaubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 // not done yet
 
-size_t	ft_strlcat(char *dest, const char *src, size_t dest_size)
+size_t	ft_strlcat(char *dest, const char *src, size_t siz)
 {
 	size_t	i;
 	size_t	src_len;
 	size_t	dest_len;
+	size_t	j;
 
 	i = 0;
+	j = 0;
 	src_len = ft_strlen(src);
 	dest_len = ft_strlen(dest);
-	if (!dest)
-		return (0);
-	if (!src)
-		return (0);
-	if (dest_len <= dest_size)
-		dest_len = dest_size;
-	if (dest_len == dest_size)
-		return (dest_len + src_len);
-	while (src[i] && *(size_t *)(dest + i) < (dest_size - 1))
+	if (siz == 0)
+		return (src_len);
+	if (siz <= dest_len)
+		return (src_len + siz);
+	while (dest[j] && j < siz)
+		j++;
+	while (src[i] && (i + j < siz - 1))
 	{
 		dest[dest_len + i] = src[i];
 		i++;
 	}
-	return (src_len + dest_len);
+	if (j + i < siz)
+	dest[j + i] = '\0';
+	return (src_len + j);
 }
